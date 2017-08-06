e7f4ee2d73afb0dc8f616126c37a337b0e90fd94
Markandeya <vasalpha@gmail.com>
2017-08-06 21:05:13 +0530

    Fixes redirection of user and admin guard

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('admin')->group(function() {
+Route::prefix('/admin')->group(function() {
   Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
   Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
   Route::get('/', 'AdminController@index')->name('admin.dashboard');
