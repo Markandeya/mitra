55925fb17083b4e66134d7a8180a1ac52452d0c1
Markandeya <vasalpha@gmail.com>
2017-08-25 15:53:23 +0530

    Fix box-textarea overflow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   </div>
                   <div class="col-md-2">
-                    <div class="" data-spy="affix" data-offset-top="2px">
+                    <div class="hidden-xs" data-spy="affix" data-offset-top="2px">
                       <div class="box-shadow-sidebar"><h3>Online members</h3></div>
                     </div>
                   </div>
